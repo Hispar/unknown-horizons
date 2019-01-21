91f199612952e7a82b6c2c2dc010acc43e22b425
pburgos <pburgos@mrmilu.com>
2019-01-21 15:40:42 +0100

    Add configuration settings to run script

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+mkdir ./.config/unknown-horizons/
+cp ./content/settings-template.xml ./.config/unknown-horizons/settings.xml
+
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
